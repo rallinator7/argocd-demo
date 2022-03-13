a3b1071b21dc69f576799a32dc4a1623ff66d3c4
rallinator7 <rallinator7@gmail.com>
2022-03-13 15:27:49 -0500

    feat: updates argo-app to reference new manifest repo

--- scripts/setup-argo.sh
+++ scripts/setup-argo.sh
@@ -6,7 +6,7 @@ CRED_TEMP_FILE="$ARGO_DIR/templates/argocd-configs/repo-creds.yaml"
 CRED_FILE="$ARGO_DIR/templates/argocd-configs/repo-creds.enc.yaml"
 PRIVATE_KEY=$(yq e '.argo.privateKey' $CONFIG_FILE)
 BASE_REPO=$(yq e '.argo.baseRepo' $CONFIG_FILE)
-FULL_REPO=$(yq e '.argo.fullRepo' $CONFIG_FILE)
+MANIFEST_REPO=$(yq e '.argo.manifestRepo' $CONFIG_FILE)
 ARGO_PWD=$(yq e '.argo.adminPassword' $CONFIG_FILE)
 BCRYPT_PWD=$(htpasswd -nbBC 10 "" $ARGO_PWD | tr -d ':\n' | sed 's/$2y/$2a/')
 SOPS_PUBLIC_KEY=$(yq e '.sops.publicKey' $CONFIG_FILE)
@@ -29,7 +29,7 @@ spec:
         url: 
       data:
         sshPrivateKey: 
-    - name: demo-repo
+    - name: demo-manifest-repo
       labels:
         argocd.argoproj.io/secret-type: repository
       stringData:
@@ -39,7 +39,7 @@ EOF
 
 url="$BASE_REPO" yq e '.spec.secretTemplates[0].stringData.url = env(url)' -i $CRED_TEMP_FILE
 key="$PRIVATE_KEY" yq e '.spec.secretTemplates[0].data.sshPrivateKey = env(key)' -i $CRED_TEMP_FILE
-url="$FULL_REPO" yq e '.spec.secretTemplates[1].stringData.url = env(url)' -i $CRED_TEMP_FILE
+url="$MANIFEST_REPO" yq e '.spec.secretTemplates[1].stringData.url = env(url)' -i $CRED_TEMP_FILE
 
 sops -e --age $SOPS_PUBLIC_KEY  --encrypted-suffix='Templates' $CRED_TEMP_FILE > $CRED_FILE
 rm $CRED_TEMP_FILE
